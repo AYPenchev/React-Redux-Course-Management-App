0b0ae45712889ea7959ec4d18a0736b7ad1d014d
Asen Penchev <asen.penchev@grafixoft.com>
2020-04-05 14:42:38 -0400

    Testing with Jest

---
+++ tools/fiileMock.js
@@ -0,0 +1,2 @@
+// Mocks file imports for Jest. As suggested by https://jestjs.io/dock/en/webpack
+module.exports = "test-file-stub";
---
+++ tools/styleMock.js
@@ -0,0 +1,2 @@
+// Mocks CSS imports for Jest. As suggested by https://jestjs.io/dock/en/webpack
+module.exports = {};
